2c4aba2921b8940a196c400cd30c0df9a32ff6a7
long nguyen <long.ngyen91@gmail.com>
2020-03-12 18:13:46 +0700

    initial

---
+++ precache-manifest.fcd7d2c5cf962595aace4728aece6417.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3668c63364749f20ca96f35ca2e5e17c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "166e2a553fb78e79593d",
+    "url": "/static/css/main.6b2a6dc7.chunk.css"
+  },
+  {
+    "revision": "959562e6603b42fb69a8",
+    "url": "/static/js/2.8220c309.chunk.js"
+  },
+  {
+    "revision": "8023c0ee2254c66f01261c1520f25848",
+    "url": "/static/js/2.8220c309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "166e2a553fb78e79593d",
+    "url": "/static/js/main.6a975e60.chunk.js"
+  },
+  {
+    "revision": "d62d158459d89b3d5aa9",
+    "url": "/static/js/runtime-main.ed484afd.js"
+  },
+  {
+    "revision": "5ec46c9b934d1d2d1cc87faf36af3607",
+    "url": "/static/media/apac.5ec46c9b.png"
+  },
+  {
+    "revision": "d40fc012af568b854a6748a1916aed12",
+    "url": "/static/media/avatar.d40fc012.jpg"
+  },
+  {
+    "revision": "4af08055746c855ca4e8ba66187646fb",
+    "url": "/static/media/bbaghome.4af08055.jpg"
+  },
+  {
+    "revision": "afebdd03850daa139ba858b6bdc2d3e8",
+    "url": "/static/media/bbagshop.afebdd03.jpg"
+  },
+  {
+    "revision": "74e624702c404f643b40d21143238dac",
+    "url": "/static/media/bbagsingle.74e62470.jpg"
+  },
+  {
+    "revision": "c18174b253b17a6cf9ed4ba585285c8b",
+    "url": "/static/media/bemohome.c18174b2.jpg"
+  },
+  {
+    "revision": "d106462737b0ec361aa8b0edccbe1660",
+    "url": "/static/media/bemosingle.d1064627.jpg"
+  },
+  {
+    "revision": "ff31482ff794352e72871a5dca9e653d",
+    "url": "/static/media/bissblock.ff31482f.jpg"
+  },
+  {
+    "revision": "bcc98a97fd813a46e6bcd34490e4304b",
+    "url": "/static/media/bisshome.bcc98a97.jpg"
+  },
+  {
+    "revision": "961ccb8eba8e9ac78fc180a0b3e22f3c",
+    "url": "/static/media/bissproject.961ccb8e.jpg"
+  },
+  {
+    "revision": "e33b915992d0225776118ff0896e6744",
+    "url": "/static/media/budform.e33b9159.jpg"
+  },
+  {
+    "revision": "4e2376388dcdca8e897cf43981244627",
+    "url": "/static/media/budhome.4e237638.jpg"
+  },
+  {
+    "revision": "b7454f456fa015426411712b762be142",
+    "url": "/static/media/chatmessage.b7454f45.jpg"
+  },
+  {
+    "revision": "f09e7eb489767b53775e565ed30a130e",
+    "url": "/static/media/chatshop.f09e7eb4.jpg"
+  },
+  {
+    "revision": "40639bd844a1104857630f4c83059915",
+    "url": "/static/media/chatsignin.40639bd8.jpg"
+  },
+  {
+    "revision": "6135561e7b4101fdb716740942229a9e",
+    "url": "/static/media/chattme.6135561e.jpg"
+  },
+  {
+    "revision": "a7e80ec711ae2742361fc541838c1176",
+    "url": "/static/media/cv.a7e80ec7.docx"
+  },
+  {
+    "revision": "c0bbeff2a53b8bd124992f19005b66f7",
+    "url": "/static/media/doorigo_form.c0bbeff2.jpg"
+  },
+  {
+    "revision": "f0c42e4d9e34202120304f1acd2130e7",
+    "url": "/static/media/doorigo_home.f0c42e4d.jpg"
+  },
+  {
+    "revision": "120bbcab99ba8b40967bafed34b853f4",
+    "url": "/static/media/jlshome.120bbcab.jpg"
+  },
+  {
+    "revision": "2fc3d9740a99ec563371ee50cfeb66d3",
+    "url": "/static/media/khtn.2fc3d974.png"
+  },
+  {
+    "revision": "8ff4837c6b7c729cb34d4b63f7f9637f",
+    "url": "/static/media/mof.8ff4837c.png"
+  },
+  {
+    "revision": "494d066178d7cc384e649a00564c8f62",
+    "url": "/static/media/stmath_form.494d0661.jpg"
+  },
+  {
+    "revision": "eb6483df66c051146922e488ae91b16c",
+    "url": "/static/media/visahuntadmin.eb6483df.jpg"
+  },
+  {
+    "revision": "7dbdc2746cbcab7ec7967fbf1d7e71d8",
+    "url": "/static/media/visahuntform.7dbdc274.jpg"
+  },
+  {
+    "revision": "4dd6da953fe64441b7552d703ee20d5e",
+    "url": "/static/media/visahunthome.4dd6da95.jpg"
+  },
+  {
+    "revision": "014cbaa5bbc045b5923168d74a5e01ba",
+    "url": "/static/media/xappadminhome.014cbaa5.jpg"
+  },
+  {
+    "revision": "a4cbcf70b80dd0d6a4ebe83a55d5fd5e",
+    "url": "/static/media/xappadminlogin.a4cbcf70.jpg"
+  },
+  {
+    "revision": "5e00ccf92eb09a3e06bf4871e3e048c4",
+    "url": "/static/media/xappclient.5e00ccf9.jpg"
+  },
+  {
+    "revision": "24a625ef8da1f2c0f2dcfcac4471148a",
+    "url": "/static/media/xappclienthome.24a625ef.jpg"
+  },
+  {
+    "revision": "8deb98d9f8035cd1581b66c6eb394732",
+    "url": "/static/media/xappcompany.8deb98d9.jpg"
+  }
+]);
